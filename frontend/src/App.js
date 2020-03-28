963bb1920b7b058b32b4acb382471b6fbd26b458
AlexNenartovich <aliaksandrhn@gmail.com>
2020-03-27 22:03:46 -0700

    Added landing page, and search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import AddEmployee from "./AddEmployee";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Table from "./Table";
+import LandingPage from "./LandingPage";
+import Search from "./Search";
 
 class App extends Component {
     render() {
         return (
             <Router>
               <Switch>
-                <Route exact path="/" component={AddEmployee} />
+                <Route exact path="/" component={LandingPage} />
+                  <Route exact path="/add" component={AddEmployee} />
                 <Route exact path="/view" component={Table} />
+                  <Route exact path="/search" component={Search} />
               </Switch>
             </Router>
         );
