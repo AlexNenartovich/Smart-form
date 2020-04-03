c96bdb97930a284c4ef11032b0e8e821c0c9ecf1
AlexNenartovich <aliaksandrhn@gmail.com>
2020-04-03 10:00:43 -0700

    Module modifications

---
+++ frontend/build/precache-manifest.1d06af7609e011074ff0db27a6e2ae0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f6736502bb8eb51c97e5768c1b4508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "787593d23aabaa08d0cf",
+    "url": "/static/css/main.64880057.chunk.css"
+  },
+  {
+    "revision": "53b7e5924050cc7ff397",
+    "url": "/static/js/2.3ce6349b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.3ce6349b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "787593d23aabaa08d0cf",
+    "url": "/static/js/main.5dab6082.chunk.js"
+  },
+  {
+    "revision": "10612c671883d2db00ae",
+    "url": "/static/js/runtime-main.6ac2adc3.js"
+  }
+]);
